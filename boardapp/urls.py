55c47b9a59de1e75847e90f97abc9f3bd1692412
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-01 19:44:33 +0900

    loginルーティングの設定

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signupFunc
+from .views import signupFunc, loginFunc
 
 urlpatterns = [
-    path('signup/', signupFunc),
+    path('signup/', signupFunc, name='signup'),
+    path('login/', loginFunc, name='login')
 ]

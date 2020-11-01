82e2258945755a703fc7627b624f0f089a68add9
KaiTakahashi <takahashi.329aaa@gmail.com>
2020-11-01 19:55:56 +0900

    list.htmlのルーティング設定

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import signupFunc, loginFunc
+from .views import signupFunc, loginFunc, listFunc
 
 urlpatterns = [
     path('signup/', signupFunc, name='signup'),
-    path('login/', loginFunc, name='login')
+    path('login/', loginFunc, name='login'),
+    path('list/', listFunc, name='list')
 ]
